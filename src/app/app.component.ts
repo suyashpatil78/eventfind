201102249f95525cf425ad913bb90724bd4826e2
suyashpatil78 <suyash.p@fyle.in>
2024-03-31 01:08:03 +0530

    feat: adding splash screen and icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  initializeApp(): void {
+    this.platform.ready().then(() => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      setTimeout(async () => await SplashScreen.hide(), 1000);
+    });
+  }
+
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+}
