8989c12bfc5705decf49c6300d6e873db6ab17dc
suyashpatil78 <suyashpatil505@gmail.com>
2024-07-14 20:48:34 +0530

    lcov

--- karma.conf.js
+++ karma.conf.js
@@ -25,11 +25,12 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app'),
+      dir: require('path').join(__dirname, './coverage/peach-tree'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcovonly' },
       ]
     },
     reporters: ['progress', 'kjhtml'],
